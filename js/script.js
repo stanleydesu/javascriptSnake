ff9aa841d800e88e38d85a27ca18628b0216d92e
Stanley Su <stanleysu00@gmail.com>
2017-09-05 20:54:52 +1000

    initialised canvas dimensions

---
+++ js/script.js
@@ -0,0 +1,11 @@
+"use strict";
+
+(function() {
+	const canvas = document.getElementById('canvas'),
+		  c = canvas.getContext('2d');
+
+	canvas.width = innerWidth;
+	canvas.height = innerHeight;
+
+	
+}());
