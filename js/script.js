14514817edac8b47aab12abd624e904adf6e1229
Stanley Su <stanleysu00@gmail.com>
2017-09-05 21:09:09 +1000

    added snake constructor, and listener for keypress/movement

--- js/script.js
+++ js/script.js
@@ -7,5 +7,63 @@
 	canvas.width = innerWidth;
 	canvas.height = innerHeight;
 
-	
+	function Snake(x, y, direction) {
+		this.x = x;
+		this.y = y;
+		this.direction = direction;
+		this.length = 15;
+		this.speed = this.length + 1;
+		this.move = {
+			up: () => {
+				this.y -= this.speed;
+			},
+			right: () => {
+				this.x += this.speed;
+			},
+			down: () => {
+				this.y += this.speed;
+			},
+			left: () => {
+				this.x -= this.speed;
+			},
+			'': () => {}
+		};
+		this.update = function() {
+			this.move[this.direction]();
+			this.draw();
+		};
+		this.draw = function() {
+			c.beginPath();
+			c.fillRect(this.x, this.y, this.length, this.length);
+			c.fillStyle = 'white';
+			c.fill();
+		};
+	}
+
+	const snake = new Snake(Math.floor(Math.random() * innerWidth), Math.floor(Math.random() * innerHeight), '');
+
+	window.addEventListener('keypress', function(e) {
+		let key = String.fromCharCode(e.which);
+		switch(key) {
+			case 'w':
+				snake.direction = 'up';
+				break;
+			case 'a':
+				snake.direction = 'left';
+				break;
+			case 's':
+				snake.direction = 'down';
+				break;
+			case 'd':
+				snake.direction = 'right';
+				break;
+			default:
+				break;
+		}
+	});
+
+	setInterval(function() {
+		c.clearRect(0, 0, innerWidth, innerHeight);
+		snake.update();
+	}, 60);
 }());
